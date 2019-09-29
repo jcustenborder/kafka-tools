77af7e13e48190f7ff178472d99a857ddff3566e
Jeremy Custenborder <jcustenborder@gmail.com>
2019-09-29 12:58:40 -0500

    Refactored to to sort sub commnands by name.

--- src/main/java/com/github/jcustenborder/kafka/tools/MultiCommandTool.java
+++ src/main/java/com/github/jcustenborder/kafka/tools/MultiCommandTool.java
@@ -7,6 +7,7 @@ import net.sourceforge.argparse4j.inf.Subparsers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class MultiCommandTool implements Tool {
@@ -20,12 +21,12 @@ public abstract class MultiCommandTool implements Tool {
   public void arguments(ArgumentParser parser) {
     Subparsers subparsers = parser.addSubparsers();
 
-    for (Tool tool : tools()) {
+    tools().stream().sorted(Comparator.comparing(Tool::name)).forEach(tool -> {
       Subparser subParser = subparsers.addParser(tool.name())
           .setDefault(DEST_TOOL, tool)
           .description(tool.description());
       tool.arguments(subParser);
-    }
+    });
   }
 
   @Override
